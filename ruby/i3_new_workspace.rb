5c6376745e9767146c252ad83c7865f324b17390
Sudiukil <sudiukil@gmx.fr>
2018-10-01 00:59:39 +0200

    Update

--- ruby/i3_new_workspace.rb
+++ ruby/i3_new_workspace.rb
@@ -4,6 +4,6 @@ require 'json'
 
 primary_output = JSON.parse(`i3-msg -t get_outputs`).select { |e| e['primary'] == true }.first['name']
 used = JSON.parse(`i3-msg -t get_workspaces`).map { |e| e['num'] }
-avail = ([*1..used.max]-used).first || used.max+1
+avail = ([*2..used.max]-used).first || used.max+1
 
 `i3-msg workspace #{avail}; i3-msg move workspace to output #{primary_output}`
