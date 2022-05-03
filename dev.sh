3bb1dfc9fb30a85c63fb415323e57c32359198fe
Sudiukil <contact@sudiukil.dev>
2022-05-03 15:32:26 +0200

    dev.sh improvement

--- dev.sh
+++ dev.sh
@@ -2,7 +2,7 @@
 
 # Always start from the projects directory (and make it exists/is defined)
 ! [ -d "$PROJECTS_DIR" ] && echo 'ERROR: $PROJECTS_DIR is undefined (or not a dir).' && exit 1
-cd $PROJECTS_DIR
+cd "$PROJECTS_DIR"
 
 # Prints usage for this script
 usage() {
@@ -13,26 +13,44 @@ usage() {
   echo "\t-h print this"
 }
 
+# Opens the specified project in VSCode
+# Params:
+# - project name
+main() {
+  PROJECT_NAME="$1"
+  PROJECT_PATH="$PROJECTS_DIR/$1/"
+  WORKSPACE_PATH=$(find "$PROJECT_PATH" -name "*.code-workspace" 2> /dev/null)
+
+  # Workspace path validation
+  [ -z "$WORKSPACE_PATH" ] && WORKSPACE_PATH="$PROJECT_PATH"
+  ! [ -d "$WORKSPACE_PATH" ] && echo "ERROR: '$(basename $WORKSPACE_PATH)' isn't a valid project name." && exit 1
+
+  # Start a remote session for the workspace on VSCode, using the current WSL distro
+  nohup code --remote "wsl+$WSL_DISTRO_NAME" "$WORKSPACE_PATH" > "/tmp/devsh_$PROJECT_NAME.log" 2>&1 &
+}
+
+# Creates a new project from a Git URL and open it
+# Params :
+# - git repo URL
+new() {
+  GIT_URL="$1"
+  PROJECT_NAME=$(basename "$GIT_URL" .git)
+  git clone "$GIT_URL"
+  main "$PROJECT_NAME"
+}
+
 # Parse CLI args
 parse_args() {
   case $1 in
     '') usage; exit 1;;
     -h) usage; exit 0;;
     -l) ls -1; exit 0;;
+    -n) new "$2"; exit 0;;
+    *) main "$1"; exit 0;;
   esac
 }
 
 # Parse args and look for the actual VSCode workspace
 parse_args $*
-PROJECT_NAME="$1"
-PROJECT_PATH="$PROJECTS_DIR/$1/"
-WORKSPACE_PATH=$(find $PROJECT_PATH -name "*.code-workspace" 2> /dev/null)
-
-# Workspace path validation
-[ -z "$WORKSPACE_PATH" ] && WORKSPACE_PATH=$PROJECT_PATH
-! [ -d "$WORKSPACE_PATH" ] && echo "ERROR: '$(basename $WORKSPACE_PATH)' isn't a valid project name." && exit 1
-
-# Start a remote session for the workspace on VSCode, using the current WSL distro
-nohup code --remote "wsl+$WSL_DISTRO_NAME" "$WORKSPACE_PATH" > "/tmp/devsh_$PROJECT_NAME.log" 2>&1 &
 
 exit 0
