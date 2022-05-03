9baf62a057dae3374e3b192acd508c66a1d70d63
Sudiukil <contact@sudiukil.dev>
2022-05-03 10:47:53 +0200

    Moved old scripts to archive branch

--- ruby/i3_new_workspace.rb
+++
@@ -1,9 +0,0 @@
-#!/home/quentin/.rvm/rubies/default/bin/ruby
-
-require 'json'
-
-primary_output = JSON.parse(`i3-msg -t get_outputs`).select { |e| e['primary'] == true }.first['name']
-used = JSON.parse(`i3-msg -t get_workspaces`).map { |e| e['num'] }
-avail = ([*5..used.max]-used).first || used.max+1
-
-`i3-msg workspace #{avail}; i3-msg move workspace to output #{primary_output}`
