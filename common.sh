a92b063bfe3281b2551e37a03279a53ed5c17fcd
Sudiukil <contact@sudiukil.dev>
2022-05-04 16:47:17 +0200

    Added common.sh script lib

---
+++ common.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Log error, warning and infos
+# Params
+# 1. log level (ERROR, WARN or INFO)
+# 2. log message (string)
+# 2. [optional] return code (integer)
+log() {
+  [ -z "$1" ] || [ -z "$2" ] && echo "FATAL: log error" && exit 1
+  echo "$1: $2"
+
+  [ -n "$3" ] && exit "$3"
+  case $1 in
+    "WARN") exit 1;;
+    "ERROR") exit 2;;
+  esac
+}
