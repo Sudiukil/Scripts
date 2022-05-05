e1e05fefefc0471b05b4f69724aaa21e3494d587
Sudiukil <contact@sudiukil.dev>
2022-05-05 10:04:08 +0200

    dev.sh: v2

--- common.sh
+++ common.sh
@@ -4,7 +4,7 @@
 # Params
 # 1. log level (ERROR, WARN or INFO)
 # 2. log message (string)
-# 2. [optional] return code (integer)
+# 3. [optional] return code (integer)
 log() {
   [ -z "$1" ] || [ -z "$2" ] && echo "FATAL: log error" && exit 1
   echo "$1: $2"
--- dev.sh
+++ dev.sh
@@ -1,40 +1,12 @@
 #!/bin/sh
+# shellcheck disable=SC1091
+. "$(dirname "$(readlink -f "$0")")/common.sh"
 
-# Always start from the projects directory (and make it exists/is defined)
-! [ -d "$PROJECTS_DIR" ] && echo 'ERROR: $PROJECTS_DIR is undefined (or not a dir).' && exit 1
-cd "$PROJECTS_DIR"
+# Always start from the projects directory
+cd "$PROJECTS_DIR" || log "ERROR" "PROJECTS_DIR is undefined (or not a dir)."
 
 # Check if Code is available
-! [ -x "$(which code)" ] && echo "ERROR: code binary isn't available." && exit 1
-
-# Prints usage for this script
-usage() {
-  echo "dev.sh - open a project in Visual Studio Code from a WSL shell.\n"
-  echo "Usage: dev.sh <project_name>"
-  echo "Options:"
-  echo "\t-l list known projects names"
-  echo "\t-n <git repo URL> clone a new project and open it"
-  echo "\t-p pick a project interactively"
-  echo "\t-h print this"
-}
-
-# Opens the specified project in VSCode
-# Params:
-# - project name
-main() {
-  PROJECT_NAME="$1"
-  PROJECT_PATH="$PROJECTS_DIR/$1/"
-  WORKSPACE_PATH=$(find "$PROJECT_PATH" -name "*.code-workspace" 2> /dev/null)
-
-  # Workspace path validation
-  if [ -z "$WORKSPACE_PATH" ]; then
-    WORKSPACE_PATH="$PROJECT_PATH"
-    ! [ -d "$WORKSPACE_PATH" ] && echo "ERROR: '$(basename $WORKSPACE_PATH)' isn't a valid project name." && exit 1
-  fi
-
-  # Start a remote session for the workspace on VSCode, using the current WSL distro
-  nohup code -n --remote "wsl+$WSL_DISTRO_NAME" "$WORKSPACE_PATH" > "/tmp/devsh_$PROJECT_NAME.log" 2>&1 &
-}
+[ -x "$(which code)" ] || log "ERROR" "code binary isn't available."
 
 # Creates a new project from a Git URL and open it
 # Params :
@@ -47,21 +19,52 @@ new() {
 }
 
 pick() {
-  PLIST=$(ls | cat -n)
-  echo "Available projects:\n$PLIST"
-  read -p "Type in the project number: " PNUM
-  main $(echo "$PLIST" | grep -E "^ +$PNUM" | cut -f 2)
+  # shellcheck disable=SC2012
+  PLIST=$(ls | nl -w 1 -s ' ')
+  printf "Available projects:\n%s\n" "$PLIST"
+  printf "Type in the project number: "
+  read -r PNUM
+  main "$(echo "$PLIST" | grep -E "^$PNUM " | cut -d ' ' -f 2)"
+}
+
+# Opens the specified project in VSCode
+# Params:
+# 1. project name (string)
+main() {
+  PROJECT_NAME="$1"
+  PROJECT_PATH="$PROJECTS_DIR/$1/"
+  WORKSPACE_PATH=$(find "$PROJECT_PATH" -name "*.code-workspace" 2> /dev/null)
+
+  # Workspace path validation
+  if [ -z "$WORKSPACE_PATH" ]; then
+    WORKSPACE_PATH="$PROJECT_PATH"
+    [ -d "$WORKSPACE_PATH" ] || log "ERROR" "'$(basename "$WORKSPACE_PATH")' isn't a valid project name."
+  fi
+
+  # Start a remote session for the workspace on VSCode, using the current WSL distro
+  nohup code -n --remote "wsl+$WSL_DISTRO_NAME" "$WORKSPACE_PATH" > "/tmp/devsh_$PROJECT_NAME.log" 2>&1 &
+}
+
+# Prints usage for this script
+usage() {
+  printf "dev.sh - open a project in Visual Studio Code from a WSL shell.\n
+  \rUsage: dev.sh <project_name>
+  \rOptions:
+  \t-l list known projects names
+  \t-n <git repo URL> clone a new project and open it
+  \t-p pick a project interactively
+  \t-h print this\n"
 }
 
 # Parse CLI args
 parse_args() {
   case $1 in
+    -l) ls -1;;
+    -n) new "$2";;
+    -p) pick;;
+    -h) usage;;
     '') usage; exit 1;;
-    -h) usage; exit 0;;
-    -l) ls -1; exit 0;;
-    -n) new "$2"; exit 0;;
-    -p) pick; exit 0;;
-    *) main "$1"; exit 0;;
+    *) main "$1";;
   esac
 }
 
