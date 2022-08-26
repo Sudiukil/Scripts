3ea1cff0e0c2cef8b14fd6e17963ddbe13e32de4
Sudiukil <contact@sudiukil.dev>
2022-08-26 12:14:32 +0200

    Update common.sh

--- common.sh
+++ common.sh
@@ -6,12 +6,25 @@
 # 2. log message (string)
 # 3. [optional] return code (integer)
 log() {
-  [ -z "$1" ] || [ -z "$2" ] && echo "FATAL: log error" && exit 1
+  [ -z "$1" ] || [ -z "$2" ] && log "FATAL" "Logger error"
   echo "$1: $2"
 
   [ -n "$3" ] && exit "$3"
   case $1 in
     "WARN") exit 1;;
     "ERROR") exit 2;;
+    "FATAL") exit 3;;
   esac
+}
+
+win_path_to_wsl() {
+  [ -z "$1" ] && log "INFO" "Please provide a path to convert"
+
+  printf '%s' "$1" | sed -e 's;\\;/;g' -e 's/://' -e 's;^.;/mnt/\L&;'
+}
+
+wsl_path_to_win() {
+  [ -z "$1" ] && log "INFO" "Please provide a path to convert"
+
+  printf '%s' "$1" | sed -e 's;/mnt/;;' -e 's/^./\U&:/' -e 's;/;\\;g'
 }
