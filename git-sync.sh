ac1ea4a9a8c300d72597b2352ce87fb001084bc3
Sudiukil <contact@sudiukil.dev>
2023-12-22 10:30:34 +0100

    Update

--- git-sync.sh
+++ git-sync.sh
@@ -5,19 +5,31 @@
 # - pulls all local branches
 
 # Delete all local branches that have been deleted on remote
-# Note: git snip is a custom alias, deleting all local branches that have been deleted on remote
+# Note: git snip (custom alias) deletes all local branches that do not exist on remote
 git fetch -p
 git snip
 
 # Memorize current branch
 CURRENT_BRANCH=$(git branch --show-current)
 
-# Stash local changes, temporarily
-git stash push -u
+# Stash local changes, if any
+change_count=$(git status --porcelain | wc -l)
+if [ "$change_count" -gt 0 ]; then
+  git stash push -um "[git-sync] Stashing changes before sync"
+fi
 
-# Pull all local branches
-# Note: git branches is a custom alias, listing all local branches names
-git branches | while read -r branch; do
+# Set branches list, depending on whether -a was passed
+if [ "$1" = "-a" ]; then
+  # Sync all branches
+  # Note: git branches is a custom alias, listing all local and remote branches names
+  branches=$(git branches)
+else
+  # Sync only current branch
+  branches=$(git branch -l | tr -s ' ' | cut -d ' ' -f 2)
+fi
+
+# Pull each branch
+echo "$branches" | while read -r branch; do
   git checkout "$branch"
   git pull
 done
@@ -25,7 +37,9 @@ done
 # Return to original branch
 git checkout "$CURRENT_BRANCH"
 
-# Re-apply stashed changes
-git stash pop
+# Re-apply stashed changes, if any
+if [ "$change_count" -gt 0 ]; then
+  git stash pop
+fi
 
 exit 0
