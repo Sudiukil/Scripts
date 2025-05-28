34e0485bcfaa258a5611e824e777d93189ee6acd
Sudiukil <contact@sudiukil.dev>
2025-05-28 23:24:32 +0200

    Update

--- powershell/reseticons.ps1
+++ powershell/reseticons.ps1
@@ -5,11 +5,11 @@
 taskkill /f /im explorer.exe
 
 # Clear the icons and thumbnails cache (the hard way)
-Remove-Item $env:LOCALAPPDATA\IconCache.db
+Remove-Item -Force $env:LOCALAPPDATA\IconCache.db
 Remove-Item $env:LOCALAPPDATA\Microsoft\Windows\Explorer\*cache*.db
 
 # Wait and restart explorer
 Start-Sleep 3
 Start-Process explorer.exe
 
-# Optionnal: get mad because it doesn't work for some reason
+# Optional: get mad because it doesn't work for some reason
